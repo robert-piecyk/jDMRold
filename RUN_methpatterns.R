68a24485006b699071159ffe55d37a9741943b48
R_H <hazarika.rr@gmail.com>
2020-04-28 12:53:41 +0200

    Create RUN_methpatterns.R

---
+++ RUN_methpatterns.R
@@ -0,0 +1,20 @@
+rm(list=ls())
+library(data.table)
+library(plyr)
+library(dplyr)
+library(mgcv)
+
+source(paste0(Sys.getenv("HOME"),"/basedir/DMRcaller/makeRegScripts/DMRs/methpatterns.R"))
+
+#-----------------------------------------------------------------------------
+# Extract patterns
+#-----------------------------------------------------------------------------
+
+##Output directory containing methimpute region level calls
+methout <- "/Users/rashmi/basedir/DMRcaller/test/chr1_CG"
+methpatterns(methout=methout, 
+             context=c("CG"),
+             chr <- c("chr1"),
+             #chr <- c("chr1","chr2","chr3","chr4","chr5"),
+             out.dir=methout, 
+             WT="SRR534177")
