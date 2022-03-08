c7d38cac746e5d0ddbc553cb4a70b2df02fcfd5c
R_H <hazarika.rr@gmail.com>
2022-03-08 09:53:43 +0100

    Update Run_script_jDMR.R

--- inst/extdata/Run_script_jDMR.R
+++ inst/extdata/Run_script_jDMR.R
@@ -7,7 +7,7 @@ devtools::install_github("jlab-code/jDMR")
 library(jDMR)
 
 #-------------------------------------------------------------------------------
-# Run jDMR on cytosine clusters
+# Step 1: Run jDMR on cytosine clusters
 #-------------------------------------------------------------------------------
 runjDMRregions(fasta.file="/myfolder/",
                samplefiles=system.file("extdata", "listFiles2.fn", package="jDMR"),
@@ -15,7 +15,7 @@ runjDMRregions(fasta.file="/myfolder/",
                out.dir="/myfolder/")
 
 #-------------------------------------------------------------------------------
-# Run jDMR using grid approach
+# Step 1: Run jDMR using grid approach
 #-------------------------------------------------------------------------------
 runjDMRgrid(out.dir="/myfolder/",
             fasta.file="/Annotations/TAIR10_chr_all.fa",
@@ -24,7 +24,7 @@ runjDMRgrid(out.dir="/myfolder/",
             genome="Arabidopsis")
 
 #-------------------------------------------------------------------------------
-# Generate DMR matrix
+# Step 2: Generate DMR matrix
 #-------------------------------------------------------------------------------
 makeDMRmatrix(samplefiles=system.file("extdata", "listFiles2.fn", package="jDMR"),
               input.dir="/myfolder/",
@@ -32,16 +32,17 @@ makeDMRmatrix(samplefiles=system.file("extdata", "listFiles2.fn", package="jDMR"
 
 #-------------------------------------------------------------------------------
 # Run this step only if you have multiple treatment groups and you want to
-# perform pairwise comparisons with control. This function will split the DMR matrix
-# into pairwise control-treatment groups. Don't forget to edit the metadata file before
-# running the step
+# perform pairwise comparisons with each of the treatment groups with control. 
+# This function will split the DMR matrix into pairwise control-treatment groups. 
+# Refer to section 1.2 in the manual. The metadata file requires an additional column
+# "group"
 #-------------------------------------------------------------------------------
 split.groups(samplefiles=system.file("extdata", "listFiles2.fn", package="jDMR"),
              input.dir="/myfolder/",
              out.dir="/myfolder/")
 
 #-------------------------------------------------------------------------------
-# Filter the DMR matrix
+# Step 3: Filter the DMR matrix
 #-------------------------------------------------------------------------------
 filterDMRmatrix(gridDMR=TRUE,
                 data.dir="/myfolder/",
@@ -49,21 +50,22 @@ filterDMRmatrix(gridDMR=TRUE,
                 samplefiles=system.file("extdata", "listFiles2.fn", package="jDMR"))
 
 #-------------------------------------------------------------------------------
-# Generate context-specific DMRs
+# Step 4: Generate context-specific DMRs
 #-------------------------------------------------------------------------------
 context.specific.DMRs(samplefiles=system.file("extdata", "listFiles2.fn", package="jDMR"),
                       data.dir="/myfolder/")
 
 #-------------------------------------------------------------------------------
-# Annotate DMRs
+# Step 5: Annotate DMRs. Please create a new folder and move all files to be annotated 
+# into the new folder
 #-------------------------------------------------------------------------------
-data.dir <- "/myfolder/"
+data.dir <- "/myfolder/annotate_DMRs"
 gff.AT <- "/Annotations/Arabidopsis_thaliana.TAIR10.43.gff3"
 gff.TE <- "/Annotations/TAIR10_TE.gff3"
 gff.pr <- "/Annotations/TAIR10_promoters.gff3"
 
 annotateDMRs(gff.files=c(gff.AT, gff.TE, gff.pr),
-             annotation=c("gene","promoters","TE"),
+             annotation=c("gene","TE","promoters"), #string containing annotation types
              input.dir=data.dir,
              gff3.out=FALSE,
              out.dir=data.dir)
